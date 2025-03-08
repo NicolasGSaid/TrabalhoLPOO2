70abd55b739749b54bd180f2bec401e68664c842
NicolasGSaid <nicolasgithub2@gmail.com>
2025-03-08 16:37:09 -0300

    Classe mãe pessoa

---
+++ Sistema Financeiro/src/SistemaFinanceiro/SistemaFinanceiro/Pessoa.java
@@ -0,0 +1,57 @@
+package SistemaFinanceiro;
+
+public abstract class Pessoa {
+    private int id;
+    private String nome;
+    private Endereco endereco;
+    private Telefone telefone;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Telefone getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Telefone telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void entrar() {
+        
+    }
+
+    public void imprimir() {
+        
+    }
+}
---
+++ Sistema Financeiro/src/SistemaFinanceiro/SistemaFinanceiro/Telefone.java
@@ -0,0 +1,31 @@
+package SistemaFinanceiro;
+
+public class Telefone {
+    private int ddd;
+    private long numero;
+
+    public int getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(int ddd) {
+        this.ddd = ddd;
+    }
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    public void entrar() {
+      
+    }
+
+    public void imprimir() {
+       
+    }
+}
+
